0d91c4408bfdacc6db502bb381776a67295e818b
Garik <ga189212@gmail.com>
2023-11-21 14:56:57 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
+public class Main {
 
-    public class Main {
-        public static void main(String[] args) {
-            Calculator calc = Calculator.instance.get();
-            int a = calc.plus.apply(1, 2);
-            int b = calc.minus.apply(1,1);
-            int c = calc.devide.apply(a, b);
-            calc.println.accept(c);
-        }
+    public static void main(String[] args) {
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener errorListener = System.out::println;
+        Worker worker = new Worker(listener, errorListener);
+        worker.start();
+    }
 }
